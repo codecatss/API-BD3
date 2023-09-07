ab979ec4facfd66ad6589ad2145a9bd840eba3a5
Willian Danko Caboski <caboskidanko@gmail.com>
2023-09-06 22:31:41 -0300

    Construção classe Client

---
+++ backend/src/main/java/com/example/API3SEM/client/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.example.API3SEM.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Integer>{
+    
+}
---
+++ backend/src/main/java/com/example/API3SEM/client/ClientRequestDTO.java
@@ -0,0 +1,5 @@
+package com.example.API3SEM.client;
+
+public record ClientRequestDTO(String cnpj, String razao_social, String status){
+    
+}
