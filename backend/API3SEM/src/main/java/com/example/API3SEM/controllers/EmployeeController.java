c4b4849fcbebb073dbd32efc514b39933a1137ab
Laroy Prado <laroyprado@gmail.com>
2023-09-09 19:35:29 -0300

    feat: Método de listar todos os funcionários

--- backend/API3SEM/src/main/java/com/example/API3SEM/controllers/EmployeeController.java
+++ backend/API3SEM/src/main/java/com/example/API3SEM/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.API3SEM.controllers;
 
 import com.example.API3SEM.employees.Employee;
 import com.example.API3SEM.employees.EmployeeRequestDTO;
+import com.example.API3SEM.employees.EmployeeResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("employee")
@@ -33,6 +36,15 @@ public class EmployeeController {
             System.out.println(employeeData.getNome());
             throw new RuntimeException("Não foi possível Cadastrar o seu usuário, por favor verifique as informações " + e.getMessage());
         }
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<EmployeeResponseDTO> getAll(){
+
+
+        List<EmployeeResponseDTO> employeesList = repository.findAll().stream().map(EmployeeResponseDTO::new ).toList();
+        return employeesList;
     }
 
 }
