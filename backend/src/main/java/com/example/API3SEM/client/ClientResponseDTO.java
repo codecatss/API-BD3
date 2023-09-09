bd27eba18961f56333f29ea4a01a77f1cc0ad021
Laroy Prado <laroyprado@gmail.com>
2023-09-08 23:35:09 -0300

    fix: Correção de diretórios

--- backend/src/main/java/com/example/API3SEM/client/ClientResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.API3SEM.client;
-
-public record ClientResponseDTO(Integer cnpj, String razao_social, String status) {
-    
-    public ClientResponseDTO(Client client){
-        this(client.getCnpj(), client.getRazao_social(), client.getStatus());
-    }
-
-}
