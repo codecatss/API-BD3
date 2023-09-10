27a8f4812d6c58fb0de88981c515b05156714296
larissa-fernanda <laliffr@gmail.com>
2023-09-09 21:02:19 -0300

    fix: remove enums e cria superuser

--- database/database.sql
+++ database/database.sql
@@ -1,34 +1,38 @@
--- Criação do banco de dados
+-- PARTE 1: psql
+
+-- Criação do banco de dados no psql
 CREATE DATABASE "2rp";
+
+-- Configurando usuário e senha padrão para manutenção do banco de dados
+CREATE USER superuser WITH PASSWORD 'super123';
 
 -- Configurando usuário e senha padrão do banco de dados para conexão JDBC
 CREATE USER admin WITH PASSWORD 'admin123';
 
 -- Concede permissões
+GRANT ALL ON ALL TABLES IN SCHEMA public TO superuser;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public to superuser;
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO admin;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO admin;
 
--- Define o banco de dados 2rp como o padrão
-\c "2rp";
+-- PARTE 2: Dbeaver
 
 -- Criação de tabelas
-CREATE TYPE funcao_usuario_enum AS ENUM ('admin', 'gestor', 'colaborador');
-
-CREATE TYPE status_enum AS ENUM ('ativo', 'inativo');
 
 CREATE TABLE usuario (
     matricula VARCHAR(20) NOT NULL,
     nome VARCHAR(80) NOT NULL,
     senha VARCHAR(15) NOT NULL,
-    funcao funcao_usuario_enum NOT NULL,
-    status_usuario status_enum NOT NULL DEFAULT 'ativo',
+    funcao VARCHAR(20) NOT NULL,
+    status_usuario VARCHAR(10) NOT NULL DEFAULT 'ativo',
     PRIMARY KEY (matricula)
 );
 
 CREATE TABLE cliente (
     cnpj BIGINT NOT NULL,
     razao_social VARCHAR(150) NOT NULL,
-    status_cliente status_enum NOT NULL DEFAULT 'ativo',
+    status_cliente VARCHAR(10) NOT NULL DEFAULT 'ativo',
     PRIMARY KEY (cnpj)
 );
 
@@ -36,7 +40,7 @@ CREATE TABLE centro_resultado (
     nome VARCHAR(30) NOT NULL,
     codigo_cr VARCHAR(10) NOT NULL,
     sigla VARCHAR(10) NOT NULL,
-    status_cr status_enum NOT NULL DEFAULT 'ativo',
+    status_cr VARCHAR(10) NOT NULL DEFAULT 'ativo',
     PRIMARY KEY (codigo_cr),
     UNIQUE (sigla)
 );
