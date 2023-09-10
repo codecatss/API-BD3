0c28b4d4108fe7a22825d3a9bd7ef8f11eca512e
Laroy Prado <laroyprado@gmail.com>
2023-09-10 13:51:47 -0300

    feat: Entidade Centro Resultado

---
+++ backend/API3SEM/src/main/java/com/example/API3SEM/resultCenter/CenterResultRepository.java
@@ -0,0 +1,7 @@
+package com.example.API3SEM.resultCenter;
+
+import com.example.API3SEM.employees.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CenterResultRepository extends JpaRepository<CenterResult,String> {
+}
