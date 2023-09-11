395e642792f9c4be6b5cbaacfbed34daa3c047da
Laroy Prado <laroyprado@gmail.com>
2023-09-11 14:46:07 -0300

    fix: Correção dos tipos de dados

--- backend/API3SEM/src/main/java/com/example/API3SEM/members/MemberRequestDTO.java
+++ backend/API3SEM/src/main/java/com/example/API3SEM/members/MemberRequestDTO.java
@@ -3,5 +3,5 @@ package com.example.API3SEM.members;
 import com.example.API3SEM.employees.Employee;
 import com.example.API3SEM.resultCenter.CenterResult;
 
-public record MemberRequestDTO(MemberId id,Employee employee, CenterResult centerResult,Boolean gestor) {
+public record MemberRequestDTO(String matriculaIntegrante, String codCr, Boolean gestor) {
 }
--- backend/API3SEM/src/main/java/com/example/API3SEM/members/MemberResponseDTO.java
+++ backend/API3SEM/src/main/java/com/example/API3SEM/members/MemberResponseDTO.java
@@ -3,9 +3,9 @@ package com.example.API3SEM.members;
 import com.example.API3SEM.employees.Employee;
 import com.example.API3SEM.resultCenter.CenterResult;
 
-public record MemberResponseDTO (Employee employee, CenterResult centerResult, Boolean gestor) {
+public record MemberResponseDTO(String employee, String centerResult, Boolean gestor) {
 
-    public MemberResponseDTO(Member member){
-        this(member.getEmployee(),member.getCenterResult(),member.getGestor());
+    public MemberResponseDTO(Member member) {
+        this(member.getMatriculaIntegrante(), member.getCodCr(), member.getGestor());
     }
 }
