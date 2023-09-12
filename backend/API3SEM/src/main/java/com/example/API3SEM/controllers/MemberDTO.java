180cd7d73c85e5249221b381bfa3a4a4564a75a4
Laroy Prado <laroyprado@gmail.com>
2023-09-12 16:48:22 -0300

    fix: Alteração do tipo

--- backend/API3SEM/src/main/java/com/example/API3SEM/controllers/MemberDTO.java
+++ backend/API3SEM/src/main/java/com/example/API3SEM/controllers/MemberDTO.java
@@ -2,13 +2,17 @@ package com.example.API3SEM.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberDTO {
-    private String matriculaIntegrante;
+    private List<String> matriculaIntegrante; // Agora é uma lista
     private Boolean gestor;
     private String nome; // Adicione este campo para armazenar o nome do funcionário
 }
