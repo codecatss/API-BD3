bec274886e846f05c5db2b6f840be61379e7cff6
Willian Danko Caboski <caboskidanko@gmail.com>
2023-09-11 22:17:57 -0300

    fix: table hora

--- database/database.sql
+++ database/database.sql
@@ -62,8 +62,6 @@ CREATE TABLE hora (
     data_hora_inicio TIMESTAMP NOT NULL,
     data_hora_fim TIMESTAMP NOT NULL,
     tipo VARCHAR(20) CHECK (tipo IN ('extra', 'sobreaviso')),
-    turno VARCHAR(20) CHECK (turno IN ('diurno', 'noturno')),
-    cod_dia VARCHAR(20) CHECK (cod_dia IN ('util', 'final de semana')),
     justificativa_lancamento VARCHAR(500) NOT NULL,
     projeto VARCHAR(100) NOT NULL,
     _gestor VARCHAR(80),
@@ -72,8 +70,8 @@ CREATE TABLE hora (
     solicitante_lancamento VARCHAR(80) NOT NULL,
     excepcional BOOLEAN DEFAULT FALSE,
     aprovador_ADM VARCHAR(80),
-    FOREIGN KEY (aprovador_ADM) REFERENCES usuario(matricula), -- Replace 'usuario' with the actual table name
-    FOREIGN KEY (username_lancador) REFERENCES usuario(matricula), -- Replace 'usuario' with the actual table name
+    FOREIGN KEY (aprovador_ADM) REFERENCES usuario(matricula),
+    FOREIGN KEY (username_lancador) REFERENCES usuario(matricula),
     FOREIGN KEY (cod_cr) REFERENCES centro_resultado(codigo_cr),
-    FOREIGN KEY (cnpj_cliente) REFERENCES cliente(cnpj) -- Replace 'cliente' with the actual table name
+    FOREIGN KEY (cnpj_cliente) REFERENCES cliente(cnpj)
 );
