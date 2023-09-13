2a15c45b5f7fb0322682d40288c3451643829e6c
Laroy Prado <laroyprado@gmail.com>
2023-09-13 14:07:52 -0300

    feat: Query Para Verificar se há repetiçao na tabela de CR

--- backend/API3SEM/src/main/java/com/example/API3SEM/resultCenter/CenterResultRepository.java
+++ backend/API3SEM/src/main/java/com/example/API3SEM/resultCenter/CenterResultRepository.java
@@ -1,7 +1,10 @@
 package com.example.API3SEM.resultCenter;
 
-import com.example.API3SEM.employees.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CenterResultRepository extends JpaRepository<CenterResult,String> {
+public interface CenterResultRepository extends JpaRepository<CenterResult, String> {
+    @Query("SELECT COUNT(c) > 0 FROM CenterResult c WHERE c.codigoCr = :codigoCr AND c.sigla = :sigla")
+    boolean existsByCodigoCrAndSigla(@Param("codigoCr") String codigoCr, @Param("sigla") String sigla);
 }
