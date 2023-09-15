f4afb1405959e803387c2e20dfe841ce930034ce
larissa-fernanda <laliffr@gmail.com>
2023-09-15 20:04:24 -0300

    feat: softDelete -> inativa um cr

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -38,3 +38,30 @@ function enableCenterResult(codigoCr, liElement) {
 
 
 
+function softDeleteCenterResult(codigoCr, liElement) {
+  const apiUrl = `http://localhost:8080/cr/${codigoCr}`;
+
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  fetch(apiUrl, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('Soft delete realizado com sucesso:', data);
+
+      const statusElement = liElement.querySelector('p:first-child');
+      statusElement.textContent = 'inativo';
+    })
+    .catch((error) => {
+      console.error('Erro ao realizar o soft delete:', error);
+    });
+}
