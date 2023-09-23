d8fe8cf7c1de43fedfdbdea759932f851efa4250
Laroy Prado <laroyprado@gmail.com>
2023-09-23 02:27:56 -0300

    feat: lança hora extra

--- frontend/scripts/lançamentoHora.js
+++ frontend/scripts/lançamentoHora.js
@@ -1,26 +1,70 @@
-
-
-
-
-
-const tipoHora = document.getElementById("selecionarTipoHora")
-const selecionarEmpresa = document.getElementById("selecionarEmpresa")
-const justificativaHora = document.getElementById("justificativaHora")
-const dataInicio = document.getElementById("dataInicio")
-const horaInicio = document.getElementById("horaInicio")
-const projetoHora = document.getElementById("projetoHora")
-const dataFim = document.getElementById("dataFim")
-const horaFim = document.getElementById("horaFim")
-const solicitanteHora = document.getElementById("solicitanteHora")
+const tipoHoraInput = document.getElementById("selecionarTipoHora");
+const selecionarEmpresaInput = document.getElementById("selecionarEmpresa");
+const justificativaHoraInput = document.getElementById("justificativaHora");
+const dataInicioInput = document.getElementById("dataInicio");
+const horaInicioInput = document.getElementById("horaInicio");
+const projetoHoraInput = document.getElementById("projetoHora");
+const dataFimInput = document.getElementById("dataFim");
+const horaFimInput = document.getElementById("horaFim");
+const solicitanteHoraInput = document.getElementById("solicitanteHora");
 const botaoConfirmar = document.getElementById("adicionarBotao");
 
+botaoConfirmar.addEventListener("click", () => {
+    const dataHora = lancamentoHora();
+    fazerRequisicaoPOST(dataHora);
+});
 
-const imprimirValores = () => {
+function lancamentoHora() {
+    const tipoHora = tipoHoraInput.value;
+    const selecionarEmpresa = selecionarEmpresaInput.value;
+    const justificativaHora = justificativaHoraInput.value;
+    const dataInicio = dataInicioInput.value;
+    const horaInicio = horaInicioInput.value;
+    const projetoHora = projetoHoraInput.value;
+    const dataFim = dataFimInput.value;
+    const horaFim = horaFimInput.value;
+    const solicitanteHora = solicitanteHoraInput.value;
 
-    console.log(horaFim.value)
-    console.log(tipoHora.value)
+    const dataHora = {
+        codcr: "1234",
+        lancador: "4533",
+        cnpj: "91931931",
+        data_hora_inicio: `${dataInicio}T${horaInicio}:00Z`,
+        data_hora_fim: `${dataFim}T${horaFim}:00Z`,
+        tipo: tipoHora,
+        justificativa: justificativaHora,
+        projeto: projetoHora,
+        solicitante: solicitanteHora,
+    };
+
+    console.log(dataHora);
+    return dataHora;
 }
 
+async function fazerRequisicaoPOST(dadosParaEnviar) {
 
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dadosParaEnviar),
+    };
 
-botaoConfirmar.addEventListener("click", imprimirValores);
+    try {
+
+        const response = await fetch('http://localhost:8080/hora', requestOptions);
+
+        if (!response.ok) {
+            throw new Error(`Erro na requisição: ${response.status}`);
+        }
+
+        console.log("enviou");
+
+        const data = await response.json();
+        console.log('Resposta da API:', data);
+    } catch (error) {
+
+        console.error('Erro na requisição:', error);
+    }
+}
