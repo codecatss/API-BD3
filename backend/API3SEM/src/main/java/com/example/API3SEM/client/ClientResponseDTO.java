825c9af5f5e600d4585fa891ab8d288ce6e0ef38
larissa-fernanda <laliffr@gmail.com>
2023-09-24 21:20:41 -0300

    fix: Corrige bug de cliente

--- backend/API3SEM/src/main/java/com/example/API3SEM/client/ClientResponseDTO.java
+++ backend/API3SEM/src/main/java/com/example/API3SEM/client/ClientResponseDTO.java
@@ -1,22 +1,9 @@
 package com.example.API3SEM.client;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ClientResponseDTO {
-    private String cnpj;
-    private String razaoSocial;
-    private String status;
+public record ClientResponseDTO (String cnpj, String razao_social, String status) {
 
     public ClientResponseDTO(Client client) {
-        this.cnpj = client.getCnpj();
-        this.razaoSocial = client.getRazaoSocial();
-        this.status = client.getStatus();
+        this(client.getCnpj(), client.getRazao_social(),client.getStatus());
     }
 }
