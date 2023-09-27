4c8fd56a300dfae5a38a8208b7e4a69485043886
Willian Danko Caboski <caboskidanko@gmail.com>
2023-09-26 21:32:54 -0300

    Fix: remove compoundHour

--- backend/API3SEM/src/main/java/API3SEM/entities/hora/extra/CompoundHoraDTO.java
+++
@@ -1,8 +0,0 @@
-package API3SEM.entities.hora.extra;
-
-import java.util.List;
-
-import API3SEM.DTOS.HoraDTOs;
-
-public record CompoundHoraDTO(HoraDTOs.HoraRequestDTO sobreaviso, List<HoraDTOs.HoraRequestDTO> extras) {
-}
