542d2ee8fa1201157dfa98e300918e3440731126
Willian Danko Caboski <caboskidanko@gmail.com>
2023-09-26 22:10:58 -0300

    Feat: validação Hora

--- backend/API3SEM/src/main/java/API3SEM/controllers/HoraController.java
+++ backend/API3SEM/src/main/java/API3SEM/controllers/HoraController.java
@@ -1,14 +1,21 @@
 package API3SEM.controllers;
 
 import API3SEM.DTOS.HoraDTOs;
+import API3SEM.DTOS.HoraDTOs.HoraRequestDTO;
 import API3SEM.entities.Hora;
+import API3SEM.repositories.ClientRepository;
 import API3SEM.repositories.HoraRepository;
 import API3SEM.utills.ApiException;
+import API3SEM.utills.TipoEnum;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +23,7 @@ import java.util.stream.Collectors;
 
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/hora")
 public class HoraController {
 
@@ -26,25 +34,26 @@ public class HoraController {
         this.horaRepository = horaRepository;
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @Autowired
+    private ClientRepository clientRepository;
+
     @PostMapping
     public ResponseEntity<?> criarHoraExtra(@RequestBody HoraDTOs.HoraRequestDTO horaRequestDTO) {
-        if (horaRequestDTO.data_hora_inicio().after(horaRequestDTO.data_hora_fim())) {
-            throw new ApiException("A hora de início não pode ser maior que a hora de fim.");
+        String msg = null;
+
+        if(validacaoHora(horaRequestDTO).contains("Erro")){
+            msg = "Erro: " +" "+ validacaoHora(horaRequestDTO);
+            throw new ApiException(msg);
         }
-
-        Hora hora = new Hora(horaRequestDTO);
-
-        Hora horaCriada = horaRepository.save(hora);
-
-        HoraDTOs horaResponseDTO = new HoraDTOs(horaCriada);
-
-        return new ResponseEntity<>(horaResponseDTO, HttpStatus.CREATED);
+        else{
+            saveHora(horaRequestDTO, TipoEnum.SOBREAVISO);
+            return new ResponseEntity<>(horaRequestDTO, HttpStatus.CREATED);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
     }
 
 
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public ResponseEntity<List<HoraDTOs>> listarHoras() {
         List<Hora> horas = horaRepository.findAll();
@@ -57,4 +66,41 @@ public class HoraController {
     }
 
 
+
+
+    private String validacaoHora(HoraRequestDTO hora){
+
+        List<Timestamp> hourRange = new ArrayList<>();
+        hourRange.add(hora.data_hora_inicio());
+        hourRange.add(hora.data_hora_fim());
+
+        String cnpjCliente = hora.cnpj();
+
+        if (!clientRepository.existsById(cnpjCliente)) {
+            return "Erro: O cliente fornecido não esta cadastrado no sistema";
+        }
+        if (hourRange.get(0).after(hourRange.get(1))) {
+            return "Erro: O final da hora não pode anteceder seu início";
+        }
+        return"";
+    }
+    
+    private void saveHora(HoraRequestDTO hora, TipoEnum tipo){
+        List<Timestamp> hourRange = new ArrayList<>();
+        hourRange.add(hora.data_hora_inicio());
+        hourRange.add(hora.data_hora_fim());
+
+        Hora hour = new Hora();
+        hour.setLancador(hora.lancador());
+        hour.setData_hora_inicio(hourRange.get(0));
+        hour.setData_hora_fim(hourRange.get(1));
+        hour.setTipo(tipo.name());
+        hour.setJustificativa(hora.justificativa());
+        hour.setProjeto(hora.projeto());
+        hour.setSolicitante(hora.solicitante());
+        hour.setCodcr(hora.codcr());
+        hour.setCnpj(hora.cnpj());
+
+        horaRepository.save(hour);
+    }
 }
