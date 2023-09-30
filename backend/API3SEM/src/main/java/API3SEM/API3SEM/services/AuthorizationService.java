539066aa9d09a9171c0b49a82280d25f179bf04b
Christopher Silva <christopher.silva@courban.com.br>
2023-09-30 10:24:19 -0300

    create authorization service for spring security

---
+++ backend/API3SEM/src/main/java/API3SEM/API3SEM/services/AuthorizationService.java
@@ -0,0 +1,19 @@
+package API3SEM.API3SEM.services;
+
+import API3SEM.API3SEM.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    EmployeeRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findUserByMatricula(username);
+    }
+}
