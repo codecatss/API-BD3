d27cc74419e73f22c6f4b2b2a51242446f380f26
Christopher Silva <christopher.silva@courban.com.br>
2023-09-30 10:20:51 -0300

    add method on interface to spring security

--- backend/API3SEM/src/main/java/API3SEM/API3SEM/repositories/EmployeeRepository.java
+++ backend/API3SEM/src/main/java/API3SEM/API3SEM/repositories/EmployeeRepository.java
@@ -3,11 +3,14 @@ package API3SEM.API3SEM.repositories;
 
 import API3SEM.API3SEM.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,String> {
     List<Employee> findByMatriculaIn(List<String> matriculas);
+
+    UserDetails findUserByMatricula(String matricula);
 
     List<Employee> findByMatriculaNotIn(List<String> matriculas);
     //  List<Employees> findByNameAndMatricula(String name, String matricula);
