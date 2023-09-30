82a229a2093fea7f50a8513cf8114f161901b96e
larissa-fernanda <laliffr@gmail.com>
2023-09-29 22:38:10 -0300

    fix: Troca a máscara do enum de hora-extra

--- backend/API3SEM/src/main/java/API3SEM/utills/TipoEnum.java
+++ backend/API3SEM/src/main/java/API3SEM/utills/TipoEnum.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public enum TipoEnum {
-    SOBREAVISO, EXTRA("Hora extra");
+    SOBREAVISO, EXTRA("Hora-extra");
     private String nome;
 }
