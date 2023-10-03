2a6f30a6b68eafe80725c6cfcc3da883847daa54
Christopher Silva <christopher.silva@courban.com.br>
2023-10-03 15:44:05 -0300

    add guardian to observer user page access

--- frontend/scripts/login/guardian.js
+++ frontend/scripts/login/guardian.js
@@ -1,9 +1,25 @@
+import acessoPorFuncao from './roles.js';
+
 function checkJWTToken() {
     const token = localStorage.getItem('jwt');
 
     if (!token) {
         window.location.href = 'user.login.html';
+    } else {
+        var caminhoURL = window.location.pathname;
+        var partesCaminho = caminhoURL.split('/');
+        var nomeArquivo = partesCaminho[partesCaminho.length - 1];
+
+        var funcaoUsuario = obterFuncaoDoUsuario();
+        if (!acessoPorFuncao[funcaoUsuario].includes(nomeArquivo)) {
+            window.location.href = acessoPorFuncao[funcaoUsuario][0];
+        }
     }
 }
 
-window.addEventListener('load', checkJWTToken);
+function obterFuncaoDoUsuario() {
+    const roleUsuario = localStorage.getItem('role');
+    return roleUsuario || '';
+}
+
+window.addEventListener('DOMContentLoaded', checkJWTToken);
