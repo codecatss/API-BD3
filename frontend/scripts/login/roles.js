3e2c5d3b71233cad325155f47f213405d9627a92
Christopher Silva <christopher.silva@courban.com.br>
2023-10-03 15:43:07 -0300

    add roles object with pages name

---
+++ frontend/scripts/login/roles.js
@@ -0,0 +1,8 @@
+// roles.js
+const acessoPorFuncao = {
+    "admin": ["ADM.Cliente.html", "ADM.CR.html", "ADM.Usuario.html", "admCadastros.html"],
+    "gestor": ["clienteCadastro.html"],
+    "colaborador": ["COLABORADOR.Hora.html"]
+};
+
+export default acessoPorFuncao;
