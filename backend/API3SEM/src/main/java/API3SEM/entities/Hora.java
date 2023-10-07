8a004f7ac7ab3270d709fd88db7d11680bb96464
Laroy Prado <laroyprado@gmail.com>
2023-10-07 15:42:46 -0300

    feat:adiciona construtor de hora e coluna de lista_de_acionamentos

--- backend/API3SEM/src/main/java/API3SEM/entities/Hora.java
+++ backend/API3SEM/src/main/java/API3SEM/entities/Hora.java
@@ -2,15 +2,10 @@ package API3SEM.entities;
 
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import API3SEM.DTOS.HoraDTOs;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,6 +19,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
 
 public class Hora {
 
@@ -80,6 +76,11 @@ public class Hora {
     @Column(name = "data_modificacao_admin", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private Timestamp data_modificacao_admin;
 
+    @Transient
+    @Column(name = "lista_de_acionamentos", columnDefinition = "jsonb")
+    private List<Hora> lista_de_acionamentos;
+
+
     public Hora(HoraDTOs.HoraRequestDTO data){
         this.id = data.id();
         this.codcr = data.codcr();
@@ -101,5 +102,24 @@ public class Hora {
     }
 
 
-
+    public Hora(Hora hora) {
+        this.id = hora.getId();
+        this.codcr = hora.getCodcr();
+        this.lancador = hora.getLancador();
+        this.cnpj = hora.getCnpj();
+        this.data_hora_inicio = hora.getData_hora_inicio();
+        this.data_hora_fim = hora.getData_hora_fim();
+        this.tipo = hora.getTipo();
+        this.justificativa = hora.getJustificativa();
+        this.projeto = hora.getProjeto();
+        this.solicitante = hora.getSolicitante();
+        this.status_aprovacao = hora.getStatus_aprovacao();
+        this.justificativa_negacao = hora.getJustificativa_negacao();
+        this.matricula_gestor = hora.getMatricula_gestor();
+        this.data_lancamento = hora.getData_lancamento();
+        this.data_modificacao_gestor = hora.getData_modificacao_gestor();
+        this.matricula_admin = hora.getMatricula_admin();
+        this.data_modificacao_admin = hora.getData_modificacao_admin();
+        this.lista_de_acionamentos = hora.getLista_de_acionamentos();
+    }
 }
