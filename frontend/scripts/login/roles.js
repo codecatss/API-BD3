0aa48bf45eb161fb7afa04c6822a9f36930c2983
Christopher <contato.christopher@gmail.com>
2023-10-15 05:29:07 -0300

    add pages to userRoles control

--- frontend/scripts/login/roles.js
+++ frontend/scripts/login/roles.js
@@ -1,7 +1,7 @@
 // roles.js
 const acessoPorFuncao = {
-    "admin": ["ADM.Cliente.html", "ADM.CR.html", "ADM.Usuario.html", "admCadastros.html"],
-    "gestor": ["clienteCadastro.html"],
+    "admin": ["ADM.Cliente.html", "ADM.CR.html", "ADM.Usuario.html", "admCadastros.html", "admAprovacao.html"],
+    "gestor": ["GESTOR.AprovaHora.html", "GESTOR.LancaHora.html", "gestorApontamento.html", "clienteCadastro.html"],
     "colaborador": ["COLABORADOR.Hora.html"]
 };
 
