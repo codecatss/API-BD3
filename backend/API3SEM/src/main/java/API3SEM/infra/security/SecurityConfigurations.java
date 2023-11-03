840198c7241a60bb51848b8f5baaa8b669bedead
Laroy Prado <laroyprado@gmail.com>
2023-11-03 19:20:09 -0300

    feat: adiciona novas configurações de login e token com validações

--- backend/API3SEM/src/main/java/API3SEM/infra/security/SecurityConfigurations.java
+++ backend/API3SEM/src/main/java/API3SEM/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package API3SEM.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
@@ -26,6 +31,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST,"/employee").hasRole("admin")
                                 .anyRequest().authenticated()
                         )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
