6ba34b7fd65962c4ef496fb7a00ec3d08bc5e569
Laroy Prado <laroyprado@gmail.com>
2023-11-03 11:35:53 -0300

    feat: cria service de autenticação

---
+++ backend/API3SEM/src/main/java/API3SEM/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package API3SEM.services;
+
+import API3SEM.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    EmployeeRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String matricula) throws UsernameNotFoundException {
+        return repository.findByMatricula(matricula);
+    }
+}
