ebc88206a2f378d55d797f2c71149282e27e5400
Laroy Prado <laroyprado@gmail.com>
2023-11-03 11:35:08 -0300

    feat: adiciona roles

--- backend/API3SEM/src/main/java/API3SEM/entities/funcaoUsuarioEnum/FuncaoUsuarioEnum.java
+++ backend/API3SEM/src/main/java/API3SEM/entities/funcaoUsuarioEnum/FuncaoUsuarioEnum.java
@@ -1,7 +1,17 @@
 package API3SEM.entities.funcaoUsuarioEnum;
 
 public enum FuncaoUsuarioEnum {
-    admin,
-    gestor,
-    colaborador
+    admin("admin"),
+    gestor("gestor"),
+    colaborador("colaborador");
+
+    private String role;
+
+     FuncaoUsuarioEnum(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
