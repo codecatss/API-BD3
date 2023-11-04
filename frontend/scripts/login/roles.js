8bff3f2090e79a2bb78be7d14d6783ff0a62b7aa
Christopher <contato.christopher@gmail.com>
2023-11-03 21:27:51 -0300

    create front-end

---
+++ frontend/scripts/login/roles.js
@@ -0,0 +1,8 @@
+// roles.js
+const acessoPorFuncao = {
+    "admin": ["ADM.Cliente.html", "ADM.CR.html", "ADM.Usuario.html", "admCadastros.html", "admAprovacao.html"],
+    "gestor": ["GESTOR.AprovaHora.html", "GESTOR.LancaHora.html", "gestorApontamento.html", "clienteCadastro.html"],
+    "colaborador": ["COLABORADOR.Hora.html"]
+};
+
+export default acessoPorFuncao;
